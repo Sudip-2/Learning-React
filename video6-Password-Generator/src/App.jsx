18c24ad97469b36ebc23a9985113183403638590
Sudip Paul <p452570@gmail.com>
2024-11-29 00:43:40 +0530

    made password generator more responsive

--- video6-Password-Generator/src/App.jsx
+++ video6-Password-Generator/src/App.jsx
@@ -31,19 +31,19 @@ function App() {
   return (
     <>
       <div className='w-full h-svh bg-slate-900 flex justify-center items-start'>
-        <div className='bg-slate-800 shadow-xl px-5 py-4 mt-5 rounded-lg'>
+        <div className='bg-slate-800 shadow-xl px-5 py-4 mt-5 rounded-lg w-[350px] sm:w-[500px]'>
           <h1 className='text-white text-xl font-medium text-center my-2'>Password Generator</h1>
           <div className='flex'>
             <input type="text" className='rounded-l-lg outline-none w-full px-2' readOnly value={password} ref={passwordRef}/>
             <button className='bg-blue-700 rounded-r-lg hover:bg-blue-800 text-white px-4 py-2' onClick={copyPass}>Copy</button>
           </div>
-          <div className='flex gap-1 justify-center md:gap-3 mt-2'>
-            <input type="range" min={6} max={20} value={length} onChange={(e) => setlenghth(e.target.value)} />
-            <label htmlFor="length" className='text-white'>Length {length}</label>
+          <div className='flex gap-1 justify-center sm:gap-3 mt-2'>
+            <input type="range" min={6} max={20} value={length} onChange={(e) => setlenghth(e.target.value)} className='w-[90px]'/>
+            <label htmlFor="length" className='text-white text-sm sm:text-base'>Length {length}</label>
             <input type="checkbox" defaultChecked={numberAllowed} onChange={() => setnumberAllowed((prev) => !prev)}/>
-            <label htmlFor="length" className='text-white'>Numbers</label>
+            <label htmlFor="length" className='text-white text-sm sm:text-base'>Numbers</label>
             <input type="checkbox" defaultChecked={charAllowed} onChange={() => setcharAllowed((prev) => !prev)}/>
-            <label htmlFor="length" className='text-white'>Character</label>
+            <label htmlFor="length" className='text-white text-sm sm:text-base'>Character</label>
           </div>
         </div>
       </div>
